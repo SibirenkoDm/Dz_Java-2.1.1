d1c4ee722c2250ff0227fe2f91696f2c1e1440a2
Dmitrii Sibirenko <me@sibirenko.ru>
2023-01-19 09:37:42 +0500

    revision 1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 20;
-        int b = 100;
-        int c = (b / a);
+        int коэфициент_начисления = 20;
+        int стоимость_билета = 1000;
+        int мили = (стоимость_билета / коэфициент_начисления);
 
-        System.out.println(c);
+        System.out.println(мили);
     }
 }
